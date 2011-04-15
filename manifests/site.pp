9beaf4776cbeff4d5fbe9a0ee8b22e5dfca55808
Bruno Binet <binet.bruno@gmail.com>
2011-04-15 17:44:15 +0200

    use buildout module to create new buildout environments

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,41 +2,11 @@ Exec {
     path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin"],
 }
 
-define buildout($buildoutcfg, $ensure=present) {
-    if $ensure == present {
-        exec { "mkdir -p $name":
-            unless => "test -d $name",
-        }
-        file { "$name/bootstrap.py":
-            source => "puppet:///files/zc.buildout/bootstrap.py",
-            require => Exec["mkdir -p $name"],
-        }
-        file { "$name/buildout.cfg":
-            source => $buildoutcfg,
-            require => Exec["mkdir -p $name"],
-        }
-        exec { "python bootstrap.py":
-            cwd => $name,
-            require => [File["$name/bootstrap.py"], File["$name/buildout.cfg"]],
-            unless => "test -f $name/bin/buildout",
-        }
-        exec { "$name/bin/buildout":
-            cwd => $name,
-            require => Exec["python bootstrap.py"],
-            subscribe => File["$name/buildout.cfg"],
-            refreshonly => true,
-        }
-	} else {
-		file { "$name":
-			ensure => absent,
-		}
-	}
-}
 
-
+include buildout
 class global-buildout {
-    buildout { "/usr/local/buildout-main":
-        buildoutcfg => "puppet:///files/zc.buildout/buildout-main.cfg"
+    buildout::venv { "/usr/local/buildout-main":
+        source => "puppet:///files/zc.buildout/buildout-main.cfg"
     }
 }
 
